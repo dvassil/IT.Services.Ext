91ee18ce5fa26cfb23280eb19357f2a10dcf09e7
dvassil <dvassil@users.noreply.github.com>
2021-10-10 22:19:24 +0300

    Initial commit

---
+++ src/Command.cs
@@ -0,0 +1,81 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace IT.Services.Ext
+{
+    public static class Command
+    {
+        public static void AddParameter(this IDbCommand cmd, string name,
+                                        DbType type, object value)
+        {
+            cmd.Parameters.Add(cmd.CreateParameter(name, type, ParameterDirection.Input, value));
+        }
+
+        public static void AddParameter(this IDbCommand cmd, string name,
+                                        DbType type, ParameterDirection direction,
+                                        object value)
+        {
+            cmd.Parameters.Add(cmd.CreateParameter(name, type, direction, value));
+        }
+
+        public static void AddParameter(this DbCommand cmd, string name,
+                                        DbType type, object value)
+        {
+            cmd.Parameters.Add(cmd.CreateParameter(name, type, ParameterDirection.Input, value));
+        }
+
+        public static void AddParameter(this DbCommand cmd, string name,
+                                        DbType type, ParameterDirection direction,
+                                        object value)
+        {
+            cmd.Parameters.Add(cmd.CreateParameter(name, type, direction, value));
+        }
+
+        public static void AddParameter(this DbCommand cmd, string name,
+                                        DbType type, ParameterDirection direction,
+                                        object value, bool nullable)
+        {
+            DbParameter param = cmd.CreateParameter(name, type, direction, value);
+            param.IsNullable = true;
+            cmd.Parameters.Add(param);
+        }
+
+        public static IDbDataParameter CreateParameter(this IDbCommand cmd, string name,
+                                                       DbType type, ParameterDirection direction,
+                                                       object value)
+        {
+            IDbDataParameter param = cmd.CreateParameter();
+            param.ParameterName = name;
+            param.DbType = type;
+            param.Direction = direction;
+            param.Value = value;
+
+            return param;
+        }
+
+        public static IDbDataParameter CreateParameter(this IDbCommand cmd, string name,
+                                                       DbType type, object value)
+        {
+            return cmd.CreateParameter(name, type, ParameterDirection.Input, value);
+        }
+
+        public static DbParameter CreateParameter(this DbCommand cmd, string name,
+                                                  DbType type, ParameterDirection direction,
+                                                  object value)
+        {
+            DbParameter param = cmd.CreateParameter();
+            param.ParameterName = name;
+            param.DbType = type;
+            param.Direction = direction;
+            param.Value = value;
+
+            return param;
+        }
+
+        public static DbParameter CreateParameter(this DbCommand cmd, string name,
+                                                  DbType type, object value)
+        {
+            return cmd.CreateParameter(name, type, ParameterDirection.Input, value);
+        }
+    }
+}
---
+++ src/Ext.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+
+namespace IT.Services.Ext
+{
+    public static class Ext
+    {
+        #region object Extensions
+
+        public static int ToInt(this object obj) { return Convert.ToInt32(obj); }
+
+        public static bool ToBoolean(this object obj) { return Convert.ToBoolean(obj); }
+
+        public static DateTime ToDateTime(this object obj) { return Convert.ToDateTime(obj); }
+
+        public static float ToFloat(this object obj) { return (float)Convert.ToDouble(obj); }
+
+        public static double ToDouble(this object obj) { return Convert.ToDouble(obj); }
+
+        public static decimal ToDecimal(this object obj) { return Convert.ToDecimal(obj); }
+
+        #endregion object Extensions
+
+        #region DataRow Extensions
+
+        public static T GetValue<T>(this DataRow dr, string column) { return (T)dr[column]; }
+
+        public static void GetValue(this DataRow dr, string column, out int? field)
+        {
+            if ((dr[column] == DBNull.Value) || (dr[column] == null))
+                field = null;
+            else
+                field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, string column, out int field)
+        {
+            field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, string column, out string field)
+        {
+            field = Convert.ToString(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, string column, out bool field)
+        {
+            field = Convert.ToBoolean(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, string column, out System.DateTime field)
+        {
+            field = Convert.ToDateTime(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, string column, out double field)
+        {
+            field = Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, string column, out float field)
+        {
+            field = (float)Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, string column, out decimal field)
+        {
+            field = Convert.ToDecimal(dr[column]);
+        }
+
+        public static T GetValue<T>(this DataRow dr, int column) { return (T)dr[column]; }
+
+        public static void GetValue(this DataRow dr, int column, out int field)
+        {
+            field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, int column, out string field)
+        {
+            field = Convert.ToString(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, int column, out bool field)
+        {
+            field = Convert.ToBoolean(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, int column, out System.DateTime field)
+        {
+            field = Convert.ToDateTime(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, int column, out double field)
+        {
+            field = Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, int column, out float field)
+        {
+            field = (float)Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DataRow dr, int column, out decimal field)
+        {
+            field = Convert.ToDecimal(dr[column]);
+        }
+
+        #endregion DataRow Extensions
+
+        #region DbDataReader Extensions
+
+        public static T GetValue<T>(this DbDataReader dr, string column) { return (T)dr[column]; }
+
+        public static void GetValue(this DbDataReader dr, string column, out int field)
+        {
+            field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, string column, out int? field)
+        {
+            if ((dr[column] == DBNull.Value) || (dr[column] == null))
+                field = null;
+            else
+                field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, string column, out string field)
+        {
+            field = Convert.ToString(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, string column, out bool field)
+        {
+            field = Convert.ToBoolean(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, string column, out System.DateTime field)
+        {
+            field = Convert.ToDateTime(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, string column, out double field)
+        {
+            field = Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, string column, out float field)
+        {
+            field = (float)Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, string column, out decimal field)
+        {
+            field = Convert.ToDecimal(dr[column]);
+        }
+
+        public static T GetValue<T>(this DbDataReader dr, int column) { return (T)dr[column]; }
+
+        public static void GetValue(this DbDataReader dr, int column, out int field)
+        {
+            field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, int column, out string field)
+        {
+            field = Convert.ToString(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, int column, out bool field)
+        {
+            field = Convert.ToBoolean(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, int column, out System.DateTime field)
+        {
+            field = Convert.ToDateTime(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, int column, out double field)
+        {
+            field = Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, int column, out float field)
+        {
+            field = (float)Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this DbDataReader dr, int column, out decimal field)
+        {
+            field = Convert.ToDecimal(dr[column]);
+        }
+
+        #endregion DbDataReader Extensions
+
+        #region IDataReader Extensions
+
+        public static T GetValue<T>(this IDataReader dr, string column) { return (T)dr[column]; }
+
+        public static void GetValue(this IDataReader dr, string column, out short field)
+        {
+            field = Convert.ToInt16(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out int field)
+        {
+            field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out string field)
+        {
+            field = Convert.ToString(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out bool field)
+        {
+            field = Convert.ToBoolean(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out System.DateTime field)
+        {
+            field = Convert.ToDateTime(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out double? field)
+        {
+            object obj = dr[column];
+            if (obj == DBNull.Value)
+                field = null;
+            else
+                field = Convert.ToDouble(obj);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out double field)
+        {
+            field = Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out float field)
+        {
+            field = (float)Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, string column, out decimal field)
+        {
+            field = Convert.ToDecimal(dr[column]);
+        }
+
+        public static T GetValue<T>(this IDataReader dr, int column) { return (T)dr[column]; }
+
+        public static void GetValue(this IDataReader dr, int column, out int field)
+        {
+            field = Convert.ToInt32(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, int column, out string field)
+        {
+            field = Convert.ToString(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, int column, out bool field)
+        {
+            field = Convert.ToBoolean(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, int column, out System.DateTime field)
+        {
+            field = Convert.ToDateTime(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, int column, out double field)
+        {
+            field = Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, int column, out float field)
+        {
+            field = (float)Convert.ToDouble(dr[column]);
+        }
+
+        public static void GetValue(this IDataReader dr, int column, out decimal field)
+        {
+            field = Convert.ToDecimal(dr[column]);
+        }
+
+        #endregion IDataReader Extensions
+
+    }
+}
